46e3b5aa22ec93fc4d52d94f6835952c5374737c
JimSeker <seker@uwyo.edu>
2015-05-07 09:06:02 -0600

    moved gpsDemo here.  Also updated to API 22

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Input and input2 demo the keyboard, touch, guesture listeners
 
 pitchrollX demo the sensors in the device, like accelerometer, meagnetic_fild (pitchroll3), orientation (pitchroll)
 
+gpsDemo is an example of how to use the location/gps on the phone.
 
 These are example code for University of Wyoming, Cosc 4730 Mobile Programming course.
 All examples are for Android.
---
+++ eclipse/gpsDemo/src/edu/cs4730/gpsDemo/MainFragment.java
@@ -0,0 +1,99 @@
+package edu.cs4730.gpsDemo;
+
+import java.util.List;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * this is a demo to show how to get location (ie gps) information from the device.
+ * shows a listener changes and then just get the current (last known) information at the bottom.
+ * 
+ * if using the emulator, set the location in the emulator before running this code, otherwise
+ * you may get errors or just really odd results.
+ */
+public class MainFragment extends Fragment {
+	TextView output;
+	public MainFragment() {
+		// Required empty public constructor
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View myView = inflater.inflate(R.layout.fragment_main, container, false);
+
+		output = (TextView) myView.findViewById(R.id.TextView01);
+		output.append("\nNOTE, if you haven't told the Sim a location, there will be errors!\n");
+		LocationManager myL = (LocationManager) getActivity().getBaseContext().getSystemService(Context.LOCATION_SERVICE);
+		//in the activity use:
+		//LocationManager myL = (LocationManager) getBaseContext().getSystemService(Context.LOCATION_SERVICE);
+		//In other places may need to use (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+		//add a location listener, here building on the fly.
+		myL.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+				new LocationListener() {
+			@Override
+			public void onLocationChanged(Location location) {
+				//if we have location information, update the screen here.  just lat and lot, others
+				//are shown if you may need them.
+				if (location != null) {
+					output.append("\n onLocationChanged called");
+					/*	        location.getAltitude();
+	    			        location.getLatitude();
+	    			        location.getLongitude();
+	    			        location.getTime();
+	    			        location.getAccuracy();
+	    			        location.getSpeed();
+	    			        location.getProvider();
+					 */
+					output.append("\n"+location.getLatitude() + " "+ location.getLongitude());
+
+				}
+			}
+			@Override
+			public void onProviderDisabled(String provider) {
+
+			}
+			@Override
+			public void onProviderEnabled(String provider) {
+
+
+			}
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+
+
+			}
+		} );
+
+		//Get a list of providers
+		//could also use  String = getBestProvider(Criteria  criteria, boolean enabledOnly)
+		List<String> mylist = myL.getProviders(true);
+		Location loc = null;  String networkstr = "";
+		for (int i=0; i<mylist.size() && loc ==null ; i++) {
+			networkstr = mylist.get(i).toString();
+			output.append("\n Attempting: "+ networkstr);
+			loc = myL.getLastKnownLocation(networkstr);   
+		}
+		if (loc != null ) {
+			double sLatitude = loc.getLatitude();
+			double sLongitude = loc.getLongitude();
+			String location = " "+sLatitude+","+sLongitude; 
+			output.append(location);
+		} else {
+			output.append("\nNo location can be found.\n");
+		}
+
+		return myView;
+	}
+
+}
