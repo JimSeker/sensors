6efec124bc91a3e3aad4583be21e0131c92c6d7d
Jim Ward <JimSeker@users.noreply.github.com>
2023-08-31 10:21:26 -0600

    added kotlin version

--- README.md
+++ README.md
@@ -10,21 +10,23 @@ Android Hardware Sensor and input Examples
 
 <b>input2_tk</b> (kotlin) demo the keyboard, touch, gesture listeners using the whole screen.
 
-<b>pitchroll</b> demo the sensors in the device with orientation
+<b>pitchroll</b> (java) demo the sensors in the device with orientation
 
-<b>pitchroll_tk</b> demo the sensors in the device with orientation
+<b>pitchroll_tk</b> (kotlin) demo the sensors in the device with orientation
 
-<b>pitchroll2</b> demo the sensors in the device with accelerometer, meagnetic_field 
+<b>pitchroll2</b> (java) demo the sensors in the device with accelerometer, meagnetic_field 
 
-<b>pitchroll2_tk</b> demo the sensors in the device with accelerometer, meagnetic_field 
+<b>pitchroll2_tk</b> (kotlin) demo the sensors in the device with accelerometer, meagnetic_field 
 
-<b>pitchroll3</b> demo the sensors in the device with accelerometer, meagnetic_field variant from 2
+<b>pitchroll3</b> (java) demo the sensors in the device with accelerometer, meagnetic_field variant from 2
 
-<b>pitchroll3_tk</b> demo the sensors in the device with accelerometer, meagnetic_field  variant from 2
+<b>pitchroll3_tk</b> (kotlin) demo the sensors in the device with accelerometer, meagnetic_field  variant from 2
 
 <b>8ball</b> is a demo using the magic 8 ball example.  It use the linear acceleration to determine if the "ball" is being shaken.
 
-<b>gpsDemo</b> is an example of how to use the location/gps on the phone.
+<b>gpsDemo</b> (java)is an example of how to use the location/gps on the phone.
+
+<b>gpsDemo</b> (kotlin) is an example of how to use the location/gps on the phone.
 
 <b>BiometricPromptDemo</b> is an API 28 version to use fingerprints (and more later if the phones support more biometrics).  It needs some work, but is a functional example.  For the fingerprint demos, see legacy.
 
---
+++ gpsDemo_kt/app/src/main/java/edu/cs4730/gpsdemo_kt/MainActivity.kt
@@ -0,0 +1,218 @@
+package edu.cs4730.gpsdemo_kt
+
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import edu.cs4730.gpsdemo_kt.databinding.ActivityMainBinding
+
+
+/**
+ * this is a demo to show how to get location (ie gps) information from the device.
+ * shows a listener changes and then just get the current (last known) information at the bottom.
+ *
+ *
+ * if using the emulator, set the location in the emulator before running this code, otherwise
+ * you may get errors or just really odd results.
+ */
+class MainActivity : AppCompatActivity() {
+    var TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
+    var myL: LocationManager? = null
+    private lateinit var REQUIRED_PERMISSIONS: Array<String>
+    private var rpl: ActivityResultLauncher<Array<String>>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //full set of permissions, both coarse and fine
+        REQUIRED_PERMISSIONS = arrayOf(
+            "android.permission.ACCESS_FINE_LOCATION", "android.permission.ACCESS_COARSE_LOCATION"
+        )
+        //based on which permissions, then call the right one.
+        rpl = registerForActivityResult<Array<String>, Map<String, Boolean>>(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { isGranted ->
+            var fine = false
+            var coarse = false
+            for ((key, value) in isGranted) {
+                logthis("$key is $value")
+                if (key == "android.permission.ACCESS_FINE_LOCATION" && value) {
+                    fine = true
+                } else if (key == "android.permission.ACCESS_COARSE_LOCATION" && value) {
+                    coarse = true
+                }
+            }
+            if (fine) startGPSDemo() else if (coarse) startCoarseDemo()
+        }
+        logthis("\nNOTE, if you haven't told the Sim a location, there will be errors!\n")
+        myL = getSystemService(LOCATION_SERVICE) as LocationManager
+        startDemo()
+    }
+
+    fun startDemo() {
+        var fine = false
+        var coarse = false
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(
+                    this, permission
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                if (permission == "android.permission.ACCESS_FINE_LOCATION") {
+                    fine = true
+                } else if (permission == "android.permission.ACCESS_COARSE_LOCATION") {
+                    coarse = true
+                }
+            }
+        }
+        if (fine) startGPSDemo() else if (coarse) startCoarseDemo() else rpl!!.launch(
+            REQUIRED_PERMISSIONS
+        )
+    }
+
+    //use the GPS location for this device.
+    fun startGPSDemo() {
+
+        //first check to see if I have permissions to.  This should not be necessary.  just in case.
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            rpl!!.launch(REQUIRED_PERMISSIONS)
+        } else {
+            logthis("GPS/Fine I have permissions")
+            myL!!.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0,
+                0f,
+                object : LocationListener {
+                    override fun onLocationChanged(location: Location) {
+                        //if we have location information, update the screen here.  just lat and lot, others
+                        //are shown if you may need them.
+                        if (location != null) {
+                            logthis("Fine onLocationChanged called")
+//                            location.altitude;
+//                            location.latitude;
+//                            location.longitude;
+//                            location.time;
+//                            location.accuracy;
+//                            location.speed;
+//                            location.provider;
+                            logthis(location.latitude.toString() + " " + location.longitude)
+                        }
+                    }
+
+                    override fun onProviderDisabled(provider: String) {
+                        logthis("Fine Provider is disabled")
+                    }
+
+                    override fun onProviderEnabled(provider: String) {
+                        logthis("Find Provider is enabled")
+                    }
+
+                    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+                        logthis("Fine Provider status changed")
+                    }
+                })
+
+            //Get a list of providers
+            //could also use  String = getBestProvider(Criteria  criteria, boolean enabledOnly)
+            val mylist = myL!!.getProviders(true)
+            var loc: Location? = null
+            var networkstr: String
+            var i = 0
+            while (i < mylist.size && loc == null) {
+                networkstr = mylist[i]
+                logthis("Attempting: $networkstr")
+                loc = myL!!.getLastKnownLocation(networkstr)
+                i++
+            }
+            if (loc != null) {
+                val sLatitude = loc.latitude
+                val sLongitude = loc.longitude
+                val location = " $sLatitude,$sLongitude"
+                logthis(location)
+            } else {
+                logthis("\nNo location can be found.\n")
+            }
+        }
+    }
+
+    //uses the Network location which is wifi and cell locations, instead of GPS.
+
+    fun startCoarseDemo() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            rpl!!.launch(REQUIRED_PERMISSIONS)
+        } else {
+            logthis("Coarse I have permissions")
+            myL!!.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                0,
+                0f,
+                object : LocationListener {
+                    override fun onLocationChanged(location: Location) {
+                        //if we have location information, update the screen here.  just lat and lot, others
+                        //are shown if you may need them.
+                        if (location != null) {
+                            logthis("Coarse onLocationChanged called")
+                            logthis(location.latitude.toString() + " " + location.longitude)
+                        }
+                    }
+
+                    override fun onProviderDisabled(provider: String) {
+                        logthis("Coarse Provider is disabled")
+                    }
+
+                    override fun onProviderEnabled(provider: String) {
+                        logthis("Coarse Provider is enabled")
+                    }
+
+                    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+                        logthis("Coarse Provider status changed")
+                    }
+                })
+
+            //Get a list of providers
+            //could also use  String = getBestProvider(Criteria  criteria, boolean enabledOnly)
+            val mylist = myL!!.getProviders(true)
+            var loc: Location? = null
+            var networkstr: String
+            var i = 0
+            while (i < mylist.size && loc == null) {
+                networkstr = mylist[i]
+                logthis("Attempting: $networkstr")
+                loc = myL!!.getLastKnownLocation(networkstr)
+                i++
+            }
+            if (loc != null) {
+                val sLatitude = loc.latitude
+                val sLongitude = loc.longitude
+                val location = " $sLatitude,$sLongitude"
+                logthis(location)
+            } else {
+                logthis("\nNo location can be found.\n")
+            }
+        }
+    }
+
+    //simple log function to log to both a textivew and the logcat.
+    fun logthis(item: String) {
+        binding.logger.append(item + "\n");
+        Log.d(TAG, item)
+    }
+}
