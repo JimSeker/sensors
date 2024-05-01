dd2e1900a72942e53b91546ec103da71876cdcd9
Jim Ward <JimSeker@users.noreply.github.com>
2024-05-01 10:47:05 -0600

    updated gradle, api 34, minor fixes.

--- 8ball/app/src/main/java/edu/cs4730/a8ball/EightBall.java
+++ 8ball/app/src/main/java/edu/cs4730/a8ball/EightBall.java
@@ -22,27 +22,7 @@ public class EightBall extends View {
     Paint mPaint;
     int AnswerNum = 1;
     Random myRandom = new Random();
-    String[] Answers = {
-        "Signs point to yes",
-        "Yes",
-        "Reply hazy, try again",
-        "Without a doubt",
-        "My sources say no",
-        "As I see it, yes",
-        "You may rely on it",
-        "Concentrate and ask again",
-        "Outlook not so good",
-        "It is decidedly so",
-        "Better not tell you now",
-        "Very doubtful",
-        "Yes - definitely",
-        "It is certain",
-        "Cannot predict now",
-        "Most likely",
-        "Ask again later",
-        "My reply is no",
-        "Outlook good",
-        "Don\'t count on it"};
+    String[] Answers = {"Signs point to yes", "Yes", "Reply hazy, try again", "Without a doubt", "My sources say no", "As I see it, yes", "You may rely on it", "Concentrate and ask again", "Outlook not so good", "It is decidedly so", "Better not tell you now", "Very doubtful", "Yes - definitely", "It is certain", "Cannot predict now", "Most likely", "Ask again later", "My reply is no", "Outlook good", "Don't count on it"};
 
     public EightBall(Context context) {
         super(context);
