db7fb3cf5dc7a8d799e2444aaeaaef041e5c3efd
Jim Ward <JimSeker@users.noreply.github.com>
2024-11-04 15:35:48 -0700

    updates to api 35

--- nfc/NFCreadDemo/app/src/main/java/edu/cs4730/nfcreaddemo/MainActivity.java
+++ nfc/NFCreadDemo/app/src/main/java/edu/cs4730/nfcreaddemo/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.cs4730.nfcreaddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import android.content.Intent;
 import android.nfc.NdefMessage;
@@ -30,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         logger = findViewById(R.id.logger);
     }
 
